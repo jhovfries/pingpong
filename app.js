3588a13dff6149aa7b97c599acdee53cddc692ec
jhovfries <jhovanariverab@gmail.com>
2024-06-11 17:25:58 -0600

    refactoring Score Keeper

--- app.js
+++ app.js
@@ -1,3 +1,70 @@
+const p1={
+    score:0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
+const p2={
+    score:0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
+
+const resetButton=document.querySelector('#reset');
+const winningScoreSelect=document.querySelector('#playto');
+let winningScore= 3;
+let isGameOver=false;
+
+function updateScores(player, opponent){
+    if(!isGameOver){
+        player.score+=1;
+        if(player.score===winningScore){
+            isGameOver=true;
+            player.display.classList.add('winner');
+            opponent.display.classList.add('loser');
+            player.button.disabled=true;
+            opponent.button.disabled=true;
+        }
+    player.display.textContent=player.score;
+    }
+}
+p1.button.addEventListener('click', function(){
+    updateScores(p1,p2)
+})
+p2.button.addEventListener('click', function(){
+    updateScores(p2,p1)
+})
+
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
+resetButton.addEventListener('click', reset)
+
+
+function reset() {
+    isGameOver=false;
+    for(let p of [p1,p2]){
+        p.score=0;
+        p.display.textContent=0;
+        p1.display.classList.remove('winner', 'loser');
+        p.button.disabled=false;
+    }
+
+    p1.score=0;
+    p2.score=0;
+    p1.display.textContent=0;
+    p2.display.textContent=0;
+    p1.display.classList.remove('winner', 'loser');
+    p2.display.classList.remove('winner', 'loser');
+    p1.button.disabled=false;
+    p2.button.disabled=false;
+}
+
+
+
+
+
+/*
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
@@ -58,3 +125,4 @@ function reset() {
     p1Button.disabled=false;
     p2Button.disabled=false;
 }
+*/
